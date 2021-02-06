8a0b3fc346d96384558faf7ad79ebfdc776a1b28
Amanda Riu <amanda.riu@automattic.com>
2021-02-05 22:51:19 -0500

    Styling and minWidth measurements added to TextButton

--- packages/block-library/src/search/edit.native.js
+++ packages/block-library/src/search/edit.native.js
@@ -116,6 +116,7 @@ export default function SearchEdit( {
 						onChange={ ( html ) =>
 							setAttributes( { buttonText: html } )
 						}
+						placeholder={ __( 'Add button text' ) }
 					/>
 				) }
 			</View>
--- packages/block-library/src/search/text-button.native.js
+++ packages/block-library/src/search/text-button.native.js
@@ -1,33 +1,57 @@
 /**
  * External dependencies
  */
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 /**
  * WordPress dependencies
  */
 import { RichText } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import styles from './style.scss';
 
-export default function TextButton( { value, onChange } ) {
+export default function TextButton( { value, placeholder, onChange } ) {
+    const [ textWidth, setTextWidth ] = useState( 0 );
+
     const onTextChanged = ( html ) => {
         onChange( html );
     };
 
+    const getTextWidth = ( text ) => {
+        return (
+            <Text
+                style={ styles.placeholder }
+                onTextLayout={ ( { nativeEvent } ) => {
+                    const newTextWidth =
+                        nativeEvent.lines[ 0 ] && nativeEvent.lines[ 0 ].width;
+                    if ( newTextWidth !== textWidth ) {
+                        setTextWidth( newTextWidth );
+                    }
+                } }
+            >
+                { text }
+            </Text>
+        );
+    };
+
+    const textValue = value ? value : placeholder;
     return (
-        <View style={ styles.buttonContainer }>
+        <View
+            style={ styles.buttonContainer }>
+            { getTextWidth( textValue ) }
             <RichText
-                placeholder={ __( 'Button text…' ) }
+                placeholder={ placeholder }
                 value={ value }
                 identifier="text"
                 withoutInteractiveFormatting
                 onChange={ onTextChanged }
                 textAlign="center"
+                minWidth={ textWidth }
             />
         </View>
     );
